9d4b5d2f5509adfceaf1b7bb43052c7c2e947304
tburto33 <tburto2387@gmail.com>
2021-07-22 06:52:27 -0400

    messing with tkinter in playground

--- project/other/playground.py
+++ project/other/playground.py
@@ -1,13 +1,17 @@
 # THIS IS A PLAY AROUND AREA FOR CREATING FUNCTIONS AND THINGS BEFORE IMPLEMENTATION
 # WILL NOT BE IN FINAL PROJECT
 import tkinter as tk
+# from project import character
 
 window = tk.Tk()
 window.title("DnD Character Creator")
-# greeting = tk.Label(text='Welcome to my DnD Character Creator!')
-greeting = tk.Label(text="""𝕯𝖓𝕯(5𝖊) 𝕮𝖍𝖆𝖗𝖆𝖈𝖙𝖊𝖗 𝕮𝖗𝖊𝖆𝖙𝖔𝖗""")
+greeting = tk.Label(text="𝕯𝖓𝕯(5𝖊) 𝕮𝖍𝖆𝖗𝖆𝖈𝖙𝖊𝖗 𝕮𝖗𝖊𝖆𝖙𝖔𝖗",
+                    foreground='white',
+                    background='black')
 greeting.pack()
+start = tk.Label(text="Would you like to get started? y/n")
+start_input = tk.Entry()
+start.pack()
+start_input.pack()
 
 window.mainloop()
-
-
